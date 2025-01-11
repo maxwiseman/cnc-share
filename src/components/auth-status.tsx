cc910a2bccf79f58ea56caa6712756b2840c69f7
maxwiseman <max@maxwiseman.io>
2025-01-10 23:42:54 -0500

    feat: update user dropdown

--- src/components/auth-status.tsx
+++ src/components/auth-status.tsx
@@ -2,6 +2,16 @@
 
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import { IconLogout } from "@tabler/icons-react";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 export default function AuthStatus() {
   const { data: session, status } = useSession();
@@ -12,10 +22,32 @@ export default function AuthStatus() {
 
   if (session) {
     return (
-      <div className="flex items-center space-x-4">
-        <span>Signed in as {session.user?.email}</span>
-        <Button onClick={() => signOut()}>Sign out</Button>
-      </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger className="rounded-full">
+          <Avatar className="h-8 w-8">
+            <AvatarImage src={session.user.image ?? ""} />
+            <AvatarFallback>
+              {session.user.name
+                ?.split(" ")
+                .map((i) => i.at(0))
+                .join("")}
+            </AvatarFallback>
+          </Avatar>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>
+            <div>{session.user.name}</div>
+            <div className="text-sm font-normal text-muted-foreground">
+              {session.user.email}
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem onSelect={() => signOut()}>
+            <IconLogout className="h-4 w-4" />
+            Sign Out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     );
   }
 
