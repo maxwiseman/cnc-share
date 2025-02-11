02c0eb320ce849e25a33905256c248b4a6ea1ad8
maxwiseman <max@maxwiseman.io>
2025-02-11 17:14:51 -0500

    chore: update home page text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import FileList from "@/components/file-list";
 
 export default function Home() {
   return (
-    <div className="space-y-8">
-      <h1 className="text-4xl font-bold">Home</h1>
+    <div className="mt-8 space-y-8">
+      <h1 className="text-center text-5xl font-bold">Files</h1>
       <FileList />
     </div>
   );
